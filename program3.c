66bab4f05970a2fd8f0f0c348b418e6ec05b2a48
Abhishekverma18028 <152985146+Abhishekverma18028@users.noreply.github.com>
2023-12-25 23:19:07 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,20 @@
+//program that uses pointer function to perform arithematic operation
+#include <stdio.h>
+int main()
+{
+    int first, second, add, subtract, multiply;
+    float divide;
+    
+    printf("Enter two integers\n");
+    scanf("%d%d", &first, &second);
+    add        = first + second;
+    subtract = first - second;
+    multiply = first * second;
+    divide     = first / (float)second;   //typecasting
+    printf("Sum = %d\n",add);
+    printf("Difference = %d\n",subtract);
+    printf("Multiplication = %d\n",multiply);
+    printf("Division = %.2f\n",divide);
+    
+return 0;
+}
